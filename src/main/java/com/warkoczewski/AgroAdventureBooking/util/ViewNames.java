972a75fe3108d8bad2999e6e1f9bc1c7a886a097
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-09-05 20:00:15 +0200

    util - ViewNames and Mappings classes, and mapTestMarker.html created

---
+++ src/main/java/com/warkoczewski/AgroAdventureBooking/util/ViewNames.java
@@ -0,0 +1,11 @@
+package com.warkoczewski.AgroAdventureBooking.util;
+
+public final class ViewNames {
+
+    public static final String MAP_TEST_MARKER = "mapTestMarker";
+    public static final String MAP = "map";
+
+
+    public ViewNames() {
+    }
+}

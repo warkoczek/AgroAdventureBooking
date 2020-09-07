f8312165ec7fda7ec6697830a355bb44a1fe3aa1
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-09-07 17:51:02 +0200

    FarmController - working on searchFarmByNamePhrase()

--- src/main/java/com/warkoczewski/AgroAdventureBooking/service/FarmService.java
+++ src/main/java/com/warkoczewski/AgroAdventureBooking/service/FarmService.java
@@ -2,9 +2,12 @@ package com.warkoczewski.AgroAdventureBooking.service;
 
 import com.warkoczewski.AgroAdventureBooking.model.Farm;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FarmService {
 
     Optional<Farm> showFarmByName(String name);
+
+    List<Farm> showFarmsByNamePhrase(String phrase);
 }
--- src/main/java/com/warkoczewski/AgroAdventureBooking/util/Mappings.java
+++ src/main/java/com/warkoczewski/AgroAdventureBooking/util/Mappings.java
@@ -6,6 +6,7 @@ public final  class Mappings {
     public static final String MARKERS = "markers";
     public static final String MARKERS_REGION = "markers/{region}";
     public static final String FARM = "farm/{name}";
+    public static final String SEARCH_FARMS = "search farms";
 
     public Mappings() {
     }

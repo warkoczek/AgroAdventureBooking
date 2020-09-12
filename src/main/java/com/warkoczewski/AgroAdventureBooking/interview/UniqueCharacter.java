77ce4211e9b6e2c91198620025978d8b58124748
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-09-12 09:00:46 +0200

    FarmServiceImplTest fixed

--- src/main/java/com/warkoczewski/AgroAdventureBooking/interview/UniqueCharacter.java
+++ src/main/java/com/warkoczewski/AgroAdventureBooking/interview/UniqueCharacter.java
@@ -5,7 +5,7 @@ public class UniqueCharacter {
 
     public static void main(String[] args){
 
-        System.out.println(getFirstUniqueCharacterIndex("georatele"));
+        System.out.println(getFirstUniqueCharacterIndex("eeoratele"));
 
     }
 
--- src/main/java/com/warkoczewski/AgroAdventureBooking/repository/FarmRepository.java
+++ src/main/java/com/warkoczewski/AgroAdventureBooking/repository/FarmRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface FarmRepository extends JpaRepository<Farm, Long> {
 
     Optional<Farm> findFarmByName(String name);

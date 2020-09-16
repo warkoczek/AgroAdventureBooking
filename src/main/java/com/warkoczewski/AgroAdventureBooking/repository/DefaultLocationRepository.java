5844a3e7d50aae0652bb50dd654246debf01a47a
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-09-16 18:25:41 +0200

    createLocation(), DuplicateCoordinatesLocation

--- src/main/java/com/warkoczewski/AgroAdventureBooking/repository/DefaultLocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.warkoczewski.AgroAdventureBooking.repository;
-
-import com.warkoczewski.AgroAdventureBooking.model.Location;
-
-public interface DefaultLocationRepository {
-
-    Long addLocation(Location location);
-}
--- src/main/java/com/warkoczewski/AgroAdventureBooking/service/LocationService.java
+++ src/main/java/com/warkoczewski/AgroAdventureBooking/service/LocationService.java
@@ -1,5 +1,6 @@
 package com.warkoczewski.AgroAdventureBooking.service;
 
+import com.warkoczewski.AgroAdventureBooking.exception.DuplicateCoordinatesException;
 import com.warkoczewski.AgroAdventureBooking.model.Location;
 import com.warkoczewski.AgroAdventureBooking.repository.LocationRepository;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,12 @@ public class LocationService {
     }
 
     public Long createLocation(Location location){
-        return locationRepository.addLocation(location);
+        locationRepository.findPointByLatAndLon(location.getLat(), location.getLon())
+                .ifPresent(location1 -> throwDuplicateCoordinatesException(location.getLat(), location.getLon()));
+        Location locationToSave = locationRepository.save(location);
+        return locationToSave.getLoc_Id();
+    }
+    private void throwDuplicateCoordinatesException(double lat, double lon){
+        throw new DuplicateCoordinatesException("Under these coordinates: " + lat + lon + " a business already exists");
     }
 }

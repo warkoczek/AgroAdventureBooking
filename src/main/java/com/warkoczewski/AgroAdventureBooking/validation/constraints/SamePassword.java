f8b99ed26434cc38016de0c3b2b148f983d15c2e
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-09-30 09:03:12 +0200

    @SamePassword constraint

--- src/main/java/com/warkoczewski/AgroAdventureBooking/validation/constraints/SamePassword.java
+++ src/main/java/com/warkoczewski/AgroAdventureBooking/validation/constraints/SamePassword.java
@@ -1,4 +1,14 @@
 package com.warkoczewski.AgroAdventureBooking.validation.constraints;
 
+import com.warkoczewski.AgroAdventureBooking.validation.validators.SamePasswordValidatorForRegistrationDataDTO;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Constraint(validatedBy = SamePasswordValidatorForRegistrationDataDTO.class)
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface SamePassword {
 }
---
+++ src/main/java/com/warkoczewski/AgroAdventureBooking/validation/validators/SamePasswordValidatorForRegistrationDataDTO.java
@@ -0,0 +1,18 @@
+package com.warkoczewski.AgroAdventureBooking.validation.validators;
+
+import com.warkoczewski.AgroAdventureBooking.dto.RegistrationDataDTO;
+import com.warkoczewski.AgroAdventureBooking.validation.constraints.SamePassword;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class SamePasswordValidatorForRegistrationDataDTO implements ConstraintValidator<SamePassword, RegistrationDataDTO> {
+
+    public void initialize(SamePassword constraint){}
+
+    @Override
+    public boolean isValid(RegistrationDataDTO registrationDataDTO, ConstraintValidatorContext context) {
+
+        return false;
+    }
+}

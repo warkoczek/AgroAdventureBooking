41482da06387e0c6892f9326e0f3c45faa9cee02
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-10-22 09:42:16 +0200

    DateService

--- src/main/java/com/warkoczewski/AgroAdventureBooking/service/DateService.java
+++ src/main/java/com/warkoczewski/AgroAdventureBooking/service/DateService.java
@@ -1,10 +1,10 @@
 package com.warkoczewski.AgroAdventureBooking.service;
 
 import com.warkoczewski.AgroAdventureBooking.dto.BookingDTO;
+import com.warkoczewski.AgroAdventureBooking.model.Date;
 
-import java.util.Date;
 import java.util.List;
 
 public interface DateService {
-    List<Date> addAll(BookingDTO bookingDTO);
+    List<Date> addBookedDays(BookingDTO bookingDTO);
 }
--- src/test/java/com/warkoczewski/AgroAdventureBooking/service/impl/DateServiceImplTest.java
+++ src/test/java/com/warkoczewski/AgroAdventureBooking/service/impl/DateServiceImplTest.java
@@ -4,18 +4,13 @@ import com.warkoczewski.AgroAdventureBooking.dto.BookingDTO;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 class DateServiceImplTest {
@@ -23,13 +18,13 @@ class DateServiceImplTest {
     private DateServiceImpl dateServiceImpl;
     @Test
     @Transactional
-    void addAllReturnListSizeOfAddedDatesEqual6() {
+    void addBookedDatesReturnListSizeOfAddedDatesEqual6() {
         //given
         BookingDTO bookingDTO = new BookingDTO(LocalDate.of(2020,11,01)
-                , LocalDate.of(2021,11, 06), "Potato Farm", "dellan");
+                , LocalDate.of(2020,11, 06), "Potato Farm", "dellan");
         int expectedSize = 6;
         //when
-        int actualSize = dateServiceImpl.addAll(bookingDTO).size();
+        int actualSize = dateServiceImpl.addBookedDays(bookingDTO).size();
         //then
         Assert.assertEquals(expectedSize, actualSize);
     }

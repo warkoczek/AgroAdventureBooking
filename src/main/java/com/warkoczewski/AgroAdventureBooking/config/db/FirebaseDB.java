1d48fd1f32db1c24b65fd30642c533eefa28c672
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-12-04 10:11:33 +0100

    db - firebase db created bean

---
+++ src/main/java/com/warkoczewski/AgroAdventureBooking/config/db/FirebaseDB.java
@@ -0,0 +1,26 @@
+package com.warkoczewski.AgroAdventureBooking.config.db;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseDB  {
+    @Bean
+    public void FirebaseDB() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("path/to/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+
+}
